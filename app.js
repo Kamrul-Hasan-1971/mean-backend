1e2dc6d148a5dcf20f63178cb7d41aaab38bd39a
Kamrul Hasan <icthasan36@gmail.com>
2023-07-05 15:15:12 +0600

    Add cors.

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
+var cors = require('cors')
 
 const postsRoutes = require("./routes/posts");
 const userRoutes = require("./routes/user");
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/images", express.static(path.join("images")));
