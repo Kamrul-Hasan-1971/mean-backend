ada1a4caef7afd6859460737e30ec7fea5826d9b
Md. Kamrul Hasan <icthasan36@gmail.com>
2023-07-09 23:30:15 +0600

    bug-fix

--- app.js
+++ app.js
@@ -1,17 +1,34 @@
 const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
-
+require('dotenv').config();
+const mongoose = require("mongoose");
+mongoose.set('strictQuery', false);
 
 const postsRoutes = require("./routes/posts");
 const userRoutes = require("./routes/user");
 
 const app = express();
 
+const connectDB= async ()=>{
+  mongoose
+  .connect(
+    "mongodb+srv://Hasan1971:"
+    +process.env.MONGO_ATLAS_PW+
+    "@cluster0.2greaqi.mongodb.net/"+process.env.DB+"?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Connected to database!");
+  })
+  .catch(() => {
+    console.log("MongoDb Connection failed!");
+  });
+}
+connectDB();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use("/images", express.static(path.join("backend/images")));
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -28,9 +45,9 @@ app.use((req, res, next) => {
 
 app.use("/api/posts", postsRoutes);
 app.use("/api/user", userRoutes);
-
 app.get("/:universalURL", (req, res) => {
   res.send("404 URL NOT FOUND");
 });
+
 
 module.exports = app;
